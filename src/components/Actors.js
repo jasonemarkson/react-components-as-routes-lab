04ec43fc05d85a49f94c932ac93b46be195d27b6
jasonemarkson <jasonmarkson76@gmail.com>
2021-08-17 20:33:52 -0400

    commit tree 3ae8ec2bdbe5ef0a5d87b94739fbf990cef49c64

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  console.log(actors)
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {
+        actors.map((actor, index) => {
+          return(
+            <div key={index}>
+              <p><strong>{actor.name}</strong></p>
+              Movies: 
+              {actor.movies.map((m, index) => {
+                return (<ul key={index}>
+                  <li>{m}</li>
+                </ul>)
+              })}
+            </div>
+          )
+        })
+      }
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,24 +3,28 @@ import { movies } from '../data';
 
 const Movies = () => {
   console.log(movies)
+  console.log("Hi")
 
   return (
     <div>
       <h1>Movies Page</h1>
           {
-          movies.forEach(movie => {
-          <div>
-            {movie.title}
-            {movie.theme}
-            {
-            movie.genres.forEach(g => {
-              <ul>
-                <li>g</li>
-              </ul>
-            })
-            }
-          </div>
-          })
+          movies.map((movie, index) => {
+          
+          return (<div key={index}>
+            <p>Title: {movie.title}</p>
+            <p>Time: {movie.time}</p>
+            
+            
+            Genres: 
+            <ul>{movie.genres.map( (g, index) => <li key={index}>
+              {g}
+              </li>)}</ul>
+            
+            
+
+          </div>)
+        })
         }
     </div>
   );
