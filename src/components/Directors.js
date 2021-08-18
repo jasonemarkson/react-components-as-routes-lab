e393427209e16974527abf7544aeb87f03ab60ba
jasonemarkson <jasonmarkson76@gmail.com>
2021-08-17 20:34:11 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  console.log(directors)
   return (
     <div>
-      {/*{code here}*/}
+    <h1>Directors Page</h1>
+      {
+        directors.map((director, index) => {
+          return (
+          <div key={index}>
+            <p><strong>{director.name}</strong></p>
+            {
+              director.movies.map((m, index) => {
+                return (<ul key={index}>
+                  <li>{m}</li>
+                </ul>)
+              })
+            }
+          </div>)
+        })
+      }
     </div>
-  );
+  )
 }
 
 export default Directors
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,8 +5,8 @@ import {
 } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
-// import Actors from '../components/Actors';
-// import Directors from '../components/Directors';
+import Actors from '../components/Actors';
+import Directors from '../components/Directors';
 import Movies from '../components/Movies';
 
 
@@ -16,8 +16,8 @@ const App = (props) => {
       <div>
         <NavBar />
         <Route exact path="/" component={Home} />
-        {/* <Route exact path="/actors" component={Actors} /> */}
-        {/* <Route exact path="/directors" component={Directors} /> */}
+        <Route exact path="/actors" component={Actors} />
+        <Route exact path="/directors" component={Directors} />
         <Route exact path="/movies" component={Movies} />
       </div>
     </Router>
